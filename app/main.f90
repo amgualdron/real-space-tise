f30607476172767dc4a8473f4f50c08e6a00261f
Andres Gualdron <an133485@ucf.edu>
2025-11-06 22:38:51 -0500

    declared variables and parameters, defined arrays necessary to use lapack, set up discrete space in x

--- app/main.f90
+++ app/main.f90
@@ -1,9 +1,39 @@
 program main 
+
+    !import modules
     use constants
     use potentials
+
     implicit none 
 
-    print *, h, hbar
+    !import lapack eigenvalue solver
+    external :: dstev
+
+    !Parameters
+    !-----------------
+
+    !number of N discrete points, increase for more precision
+    integer, parameter :: N = 5000
+    real(kind = dp), parameter :: L = 100 !in [A]
+    
+    !Variables
+    !------------------
+    real(kind = dp) :: dx, T, x_curr 
+    real(kind=dp), allocatable :: x(:)
+    integer :: i
+
+    !Lapack arrays (dstev)
+    real(kind = dp), allocatable :: D(:)    !main diagonal
+    real(kind = dp), allocatable :: K(:)    !super diagonal(and sub)
+    real(kind = dp), allocatable :: E(:)    !energy eigenvalues
+    real(kind = dp), allocatable :: psi(:,:)!eigenvectors
+
+    ! required by dstev
+    real(kind=dp), allocatable :: work(:)
+
+    ! INFO: Error flag
+    integer :: INFO
+
 
 
 end program main
